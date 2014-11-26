15239ac1e26f27c3ac8821739265826fc8844c40
Mateusz Sulima <mateusz.sulima@allegro.pl>
2014-11-26 13:55:30 +0100

    Fix for ids in CoverageAggregator

--- scalac-scoverage-plugin/src/test/scala/scoverage/CoverageAggregatorTest.scala
+++ scalac-scoverage-plugin/src/test/scala/scoverage/CoverageAggregatorTest.scala
@@ -4,7 +4,7 @@ import java.io.File
 import java.util.UUID
 
 import org.scalatest.{FreeSpec, Matchers}
-import scoverage.report.{ScoverageXmlWriter, CoverageAggregator}
+import scoverage.report.{CoverageAggregator, ScoverageXmlWriter}
 
 class CoverageAggregatorTest extends FreeSpec with Matchers {
 
@@ -20,19 +20,20 @@ class CoverageAggregatorTest extends FreeSpec with Matchers {
         source)
 
       val coverage1 = Coverage()
-      coverage1.add(Statement(source, location, 1, 155, 176, 4, "", "", "", true, 2))
+      coverage1.add(Statement(source, location, 1, 155, 176, 4, "", "", "", true, 1))
+      coverage1.add(Statement(source, location, 2, 95, 105, 19, "", "", "", false, 10))
       val dir1 = new File(IOUtils.getTempPath, UUID.randomUUID.toString)
       dir1.mkdir()
       new ScoverageXmlWriter(new File("/home/sam"), dir1, false).write(coverage1)
 
       val coverage2 = Coverage()
-      coverage2.add(Statement(source, location, 1, 95, 105, 19, "", "", "", false, 0))
+      coverage2.add(Statement(source, location, 1, 95, 105, 19, "", "", "", false, 2))
       val dir2 = new File(IOUtils.getTempPath, UUID.randomUUID.toString)
       dir2.mkdir()
       new ScoverageXmlWriter(new File("/home/sam"), dir2, false).write(coverage2)
 
       val coverage3 = Coverage()
-      coverage3.add(Statement(source, location, 2, 14, 1515, 544, "", "", "", false, 1))
+      coverage3.add(Statement(source, location, 2, 14, 1515, 544, "", "", "", false, 3))
       val dir3 = new File(IOUtils.getTempPath, UUID.randomUUID.toString)
       dir3.mkdir()
       new ScoverageXmlWriter(new File("/home/sam"), dir3, false).write(coverage3)
@@ -42,9 +43,13 @@ class CoverageAggregatorTest extends FreeSpec with Matchers {
           IOUtils.reportFile(dir2, debug = false),
           IOUtils.reportFile(dir3, debug = false))
       )
-      aggregated.statements.toSet.size shouldBe 3
-      aggregated.statements.map(_.copy(id = 0)).toSet shouldBe
-        (coverage1.statements ++ coverage2.statements ++ coverage3.statements).map(_.copy(id = 0)).toSet
+
+      aggregated.statements.map(_.copy(id=0)) should contain.theSameElementsAs(Set(
+        Statement(source, location, 0, 155, 176, 4, "", "", "", true, 1),
+        Statement(source, location, 0, 95, 105, 19, "", "", "", false, 10),
+        Statement(source, location, 0, 95, 105, 19, "", "", "", false, 2),
+        Statement(source, location, 0, 14, 1515, 544, "", "", "", false, 3)
+      ))
     }
   }
 }
